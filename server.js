d0cc8dafb8213693c8e207aa2b32a5e332a9808b
KanG98 <kylexue@Yankangs-MBP.fios-router.home>
2022-01-20 15:08:17 -0500

    added crud functions, postman testing results, inner join commands

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
 const express = require('express')
+const bodyparser = require('body-parser');
 const app = express()
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+  user: 'kylexue',
+  host: 'localhost',
+  database: 'company',
+  port: 5432,
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.post("", function (req, res){
-    // post new line of data 
-})
 
-app.get("", function (req, res){
-    // get two lines of data from db
-})
+const onboardEmployee = (req, res) => {
+    const { empl_id, dept_name } = req.body
+    pool.query('INSERT INTO departments (empl_id, dept_name) VALUES ($1, $2);', [empl_id, dept_name], (err, res) =>{
+        if(err){
+            throw err
+        }   
+    })
+    res.status(201).send(`new employee with id ${empl_id} settled into ${dept_name}`)
+}
 
-app.listen(3000)
+const getEmployeeById = (req, res) =>{
+    const { empl_id1, empl_id2 } = req.body
+    pool.query('SELECT * FROM employee WHERE empl_id = $1 OR empl_id = $2', [empl_id1, empl_id2], (error, results) => {
+        if (error) {
+            console.log(error)
+        }
+        res.status(200).json(results.rows)
+      })
+}
+
+app.post("/onboard_dpt", onboardEmployee)
+
+app.get("/employees", getEmployeeById)
+
+app.listen(8000)
