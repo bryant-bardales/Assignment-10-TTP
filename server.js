9ad9bd1fcbbfb6abf13cfb573235248a9747abff
Bryant Bardales <bryant.bardales@gmail.com>
2022-01-20 14:59:33 -0500

    Connect PSQL

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const db = require('./queries')
+const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ Message: 'Testing Assignment 10: TTP' })
 })
 
-app.post("", function (req, res){
-    // post new line of data 
-})
+//ADD REQUEST METHODS HERE
 
-app.get("", function (req, res){
-    // get two lines of data from db
-})
+// app.get('/users', db.getUsers) PLACEHOLDER
+// app.get('/users/:id', db.getUserById) PLACEHOLDER
+// app.post('/users', db.createUser) PLACEHOLDER
+// app.put('/users/:id', db.updateUser) PLACEHOLDER
+// app.delete('/users/:id', db.deleteUser) PLACEHOLDER
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
