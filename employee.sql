8ca12cf23344208145982a528a255e61610581ff
Yankang Xue <54068919+KanG98@users.noreply.github.com>
2022-01-19 23:50:16 -0500

    added postgreSQL commends for fake data

---
+++ employee.sql
@@ -0,0 +1,40 @@
+create table departments (
+	empl_id INT,
+	username VARCHAR(50),
+	gender VARCHAR(50),
+	email VARCHAR(50),
+	from_date DATE,
+	to_date DATE,
+	dept_name VARCHAR(50)
+);
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (1, 'Consuelo Robelet', 'Male', 'crobelet0@feedburner.com', '5/6/2019', '6/19/2020', 'Business Development');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (2, 'Gabe McGriele', 'Female', 'gmcgriele1@umn.edu', '5/16/2016', '7/11/2021', 'Engineering');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (3, 'Dalila Dykins', 'Female', 'ddykins2@diigo.com', '9/17/2017', '7/11/2021', 'Human Resources');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (4, 'Jocelin Goozee', 'Female', 'jgoozee3@sakura.ne.jp', '6/5/2018', '1/4/2019', 'Legal');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (5, 'Dillie Tinan', 'Male', 'dtinan4@example.com', '2/24/2019', '3/9/2018', 'Legal');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (6, 'Darell Barrable', 'Male', 'dbarrable5@miibeian.gov.cn', '12/24/2018', '5/25/2018', 'Legal');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (7, 'Debi Coan', 'Female', 'dcoan6@netscape.com', '7/31/2017', '10/4/2019', 'Business Development');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (8, 'Ashlen Beckson', 'Male', 'abeckson7@netvibes.com', '12/26/2016', '12/12/2018', 'Training');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (9, 'Filide Paiton', 'Female', 'fpaiton8@nhs.uk', '7/26/2018', '6/10/2018', 'Legal');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (10, 'Towny Broxis', 'Male', 'tbroxis9@instagram.com', '9/30/2017', '8/12/2019', 'Business Development');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (11, 'Dion Brashaw', 'Female', 'dbrashawa@t-online.de', '10/28/2018', '10/12/2020', 'Legal');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (12, 'Sukey Fransewich', 'Female', 'sfransewichb@imgur.com', '12/15/2018', '11/10/2020', 'Human Resources');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (13, 'Anny Heinz', 'Male', 'aheinzc@ow.ly', '4/27/2016', '6/10/2021', 'Product Management');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (14, 'Verla Simonutti', 'Female', 'vsimonuttid@utexas.edu', '11/21/2016', '6/28/2020', 'Engineering');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (15, 'Sanderson Abden', 'Female', 'sabdene@prlog.org', '4/18/2016', '1/27/2019', 'Support');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (16, 'Cristina Hoofe', 'Male', 'choofef@hostgator.com', '9/4/2019', '4/18/2018', 'Services');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (17, 'Estella Heffron', 'Female', 'eheffrong@abc.net.au', '9/30/2019', '5/17/2018', 'Research and Development');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (18, 'Milissent Woolford', 'Male', 'mwoolfordh@yellowpages.com', '11/4/2019', '5/29/2018', 'Human Resources');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (19, 'Billi Goakes', 'Female', 'bgoakesi@networksolutions.com', '9/21/2017', '1/27/2018', 'Training');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (20, 'Jase O''Corren', 'Male', 'jocorrenj@wp.com', '4/12/2016', '3/29/2019', 'Services');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (21, 'Gerick Springett', 'Male', 'gspringettk@noaa.gov', '6/14/2016', '10/17/2021', 'Product Management');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (22, 'Karl Mathe', 'Female', 'kmathel@wikia.com', '2/5/2018', '2/28/2018', 'Support');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (23, 'Mirella Glamart', 'Male', 'mglamartm@xing.com', '2/3/2019', '11/28/2019', 'Legal');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (24, 'Lilian Hullins', 'Female', 'lhullinsn@va.gov', '3/12/2019', '7/17/2019', 'Product Management');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (25, 'Barney Mahaffey', 'Male', 'bmahaffeyo@ihg.com', '7/10/2018', '7/22/2019', 'Support');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (26, 'Clarissa Wigginton', 'Female', 'cwiggintonp@1688.com', '7/15/2016', '11/6/2017', 'Research and Development');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (27, 'Goraud MacMechan', 'Male', 'gmacmechanq@wufoo.com', '8/11/2018', '5/14/2019', 'Services');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (28, 'Allison Moncur', 'Male', 'amoncurr@redcross.org', '12/5/2016', '10/12/2020', 'Product Management');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (29, 'Cleopatra Chastan', 'Female', 'cchastans@ezinearticles.com', '12/9/2016', '8/2/2021', 'Training');
+insert into departments (empl_id, username, gender, email, from_date, to_date, dept_name) values (30, 'Harriot Torresi', 'Male', 'htorresit@zdnet.com', '2/25/2019', '2/5/2018', 'Product Management');
+
---
+++ salaries.sql
@@ -0,0 +1,37 @@
+create table salaries (
+	empl_id INT,
+	salary INT,
+	from_date DATE,
+	to_date DATE,
+	dept_name VARCHAR(50)
+);
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('1', '$86789.94', '5/6/2019', '6/19/2020', 'Business Development');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('2', '$198265.82', '5/16/2016', '7/11/2021', 'Engineering');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('3', '$179311.84', '9/17/2017', '7/11/2021', 'Human Resources');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('4', '$103383.62', '6/5/2018', '1/4/2019', 'Legal');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('5', '$149355.21', '2/24/2019', '3/9/2018', 'Legal');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('6', '$86811.67', '12/24/2018', '5/25/2018', 'Legal');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('7', '$148438.92', '7/31/2017', '10/4/2019', 'Business Development');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('8', '$153124.95', '12/26/2016', '12/12/2018', 'Training');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('9', '$150506.53', '7/26/2018', '6/10/2018', 'Legal');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('10', '$94426.81', '9/30/2017', '8/12/2019', 'Business Development');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('11', '$188469.17', '10/28/2018', '10/12/2020', 'Legal');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('12', '$183383.77', '12/15/2018', '11/10/2020', 'Human Resources');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('13', '$136646.96', '4/27/2016', '6/10/2021', 'Product Management');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('14', '$186274.75', '11/21/2016', '6/28/2020', 'Engineering');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('15', '$151865.26', '4/18/2016', '1/27/2019', 'Support');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('16', '$83943.69', '9/4/2019', '4/18/2018', 'Services');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('17', '$145394.75', '9/30/2019', '5/17/2018', 'Research and Development');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('18', '$126253.45', '11/4/2019', '5/29/2018', 'Human Resources');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('19', '$151882.97', '9/21/2017', '1/27/2018', 'Training');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('20', '$185287.19', '4/12/2016', '3/29/2019', 'Services');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('21', '$198483.22', '6/14/2016', '10/17/2021', 'Product Management');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('22', '$197739.08', '2/5/2018', '2/28/2018', 'Support');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('23', '$156253.35', '2/3/2019', '11/28/2019', 'Legal');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('24', '$120364.05', '3/12/2019', '7/17/2019', 'Product Management');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('25', '$90069.24', '7/10/2018', '7/22/2019', 'Support');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('26', '$185454.83', '7/15/2016', '11/6/2017', 'Research and Development');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('27', '$186264.41', '8/11/2018', '5/14/2019', 'Services');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('28', '$198588.19', '12/5/2016', '10/12/2020', 'Product Management');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('29', '$82598.06', '12/9/2016', '8/2/2021', 'Training');
+insert into salaries (empl_id, salary, from_date, to_date, dept_name) values ('30', '$154725.98', '2/25/2019', '2/5/2018', 'Product Management');
